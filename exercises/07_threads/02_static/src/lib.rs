5badcb1e2c448d2502f99d72fa71de30ffbb6e56
MAKARD <themakard@icloud.com>
2026-04-07 14:26:41 +0100

    exercises/07_threads/02_static

--- exercises/07_threads/02_static/src/lib.rs
+++ exercises/07_threads/02_static/src/lib.rs
@@ -4,7 +4,17 @@
 use std::thread;
 
 pub fn sum(slice: &'static [i32]) -> i32 {
-    todo!()
+    let (left, right) = slice.split_at(slice.len() / 2);
+
+    let handle_1 = thread::spawn(|| {
+        left.iter().sum::<i32>()
+    });
+
+    let handle_2 = thread::spawn(|| {
+        right.iter().sum::<i32>()
+    });
+
+    handle_1.join().unwrap() + handle_2.join().unwrap()
 }
 
 #[cfg(test)]
